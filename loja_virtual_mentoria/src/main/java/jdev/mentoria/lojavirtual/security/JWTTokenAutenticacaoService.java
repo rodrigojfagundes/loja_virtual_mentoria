66db24c45f1ad899b76f00e8222ae19b6b909016
Rodrigo <Rodrigo@DESKTOP-NG2MGDC>
2025-05-04 13:27:23 -0300

    Configurando Spring Security e JWT com Excecoes

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -1,5 +1,6 @@
 package jdev.mentoria.lojavirtual.security;
 
+import java.io.IOException;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
 import jdev.mentoria.lojavirtual.ApplicationContextLoad;
 import jdev.mentoria.lojavirtual.model.Usuario;
 import jdev.mentoria.lojavirtual.repository.UsuarioRepository;
@@ -21,44 +23,43 @@ import jdev.mentoria.lojavirtual.repository.UsuarioRepository;
 @Service
 @Component
 public class JWTTokenAutenticacaoService {
-	
+
 	
 	private static final long EXPIRATION_TIME = 959990000;
 	private static final String SECRET = "ss/-*-*sds565dsd-s/d-s*dsds";
 	private static final String TOKEN_PREFIX = "Bearer";
 	private static final String HEADER_STRING = "Authorization";
+
 	
-	
-	public void addAuthentication(
-			HttpServletResponse response, String username) throws Exception {
+	public void addAuthentication(HttpServletResponse response, String username) throws Exception {
+
 		
-		
-	
+
 		
 		
 		
 		
-		String JWT = Jwts.builder()
-				.setSubject(username)
-				.setExpiration(new Date(
-						System.currentTimeMillis() + EXPIRATION_TIME))
+		String JWT = Jwts.builder().setSubject(username)
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
 				.signWith(SignatureAlgorithm.HS512, SECRET).compact();
+
 		
-		
-		/*Exe: Bearer *-/a*dad9s5d6as5d4s5d4s45dsd5
-		 * 4s.sd4s4d45s45d4sd54d45s4d5s.ds5d5s5d5s65d6s6d*/
+		/*
+		 * Exe: Bearer *-/a*dad9s5d6as5d4s5d4s45dsd5
+		 * 4s.sd4s4d45s45d4sd54d45s4d5s.ds5d5s5d5s65d6s6d
+		 */
 		String token = TOKEN_PREFIX + " " + JWT;
-		
-		
+
 		
 		
 		response.addHeader(HEADER_STRING, token);
 		liberacaoCors(response);
-		
-		/*Usado para ver no Postman para teste*/
+
+		/* Usado para ver no Postman para teste */
 		response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
-		
+
 	}
+
 	
 	
 	
@@ -66,47 +67,43 @@ public class JWTTokenAutenticacaoService {
 	
 	
 	
-	
-	public Authentication getAuthentication(
-			HttpServletRequest request, HttpServletResponse response) {
-		
+	public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+
 		String token = request.getHeader(HEADER_STRING);
-		if (token != null) {
-			
-			String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
-			
-			
-			String user = Jwts.parser()
-					.setSigningKey(SECRET)
-					.parseClaimsJws(tokenLimpo)
-					.getBody().getSubject();
-			
-			
-			if(user != null) {
-				Usuario usuario = ApplicationContextLoad
-						.getApplicationContext()
-						.getBean(UsuarioRepository.class)
-						.findUserByLogin(user);
+
+		try {
+
+			if (token != null) {
 				
+				String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
+
 				
-				if(usuario != null) {
-					return new UsernamePasswordAuthenticationToken(
-							usuario.getLogin(), 
-							usuario.getSenha(),
-							usuario.getAuthorities());
+				String user = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(tokenLimpo).getBody().getSubject();
+
+				if (user != null) {
+					Usuario usuario = ApplicationContextLoad.getApplicationContext().getBean(UsuarioRepository.class)
+							.findUserByLogin(user);
+
+					if (usuario != null) {
+						return new UsernamePasswordAuthenticationToken(usuario.getLogin(), usuario.getSenha(),
+								usuario.getAuthorities());
+					}
+
 				}
-				
 			}
-			
-			
+		} catch (SignatureException e) {
+			response.getWriter().write("Token esta invalido");
+		} catch (Exception e) {
+			response.getWriter().write("Token esta expirado, efetue o login novamente");
 		}
-		liberacaoCors(response);
+				
+		finally {
+			liberacaoCors(response);
+		}
 		return null;
 	}
-	
-	
-	
-	
+
 	
 	
 	
@@ -124,6 +121,5 @@ public class JWTTokenAutenticacaoService {
 			response.addHeader("Access-Control-Allow-Methods", "*");
 		}
 	}
-	
-	
+
 }
