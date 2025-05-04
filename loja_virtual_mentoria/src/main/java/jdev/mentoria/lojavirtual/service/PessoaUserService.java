ecdeccc3ef69795e7becc7e62ad2aa86eaafe585
Rodrigo <Rodrigo@DESKTOP-NG2MGDC>
2025-05-04 15:02:43 -0300

    Criando end-point marca e conta a pagar

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/service/PessoaUserService.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/service/PessoaUserService.java
@@ -192,7 +192,6 @@ public class PessoaUserService {
 		
 	}
 	
-	
 	public CepDTO consultaCep(String cep) {
 		
 		
@@ -209,6 +208,6 @@ public class PessoaUserService {
 					"https://receitaws.com.br/v1/cnpj/" + cnpj, ConsultaCnpjDto.class).getBody();
 	
 	}
-	
+
 	
 }
