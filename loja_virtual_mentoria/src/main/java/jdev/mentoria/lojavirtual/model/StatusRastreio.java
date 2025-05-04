e75c5115f33d9c2cb95a8280fb6bc757028413df
Rodrigo <Rodrigo@DESKTOP-NG2MGDC>
2025-05-04 15:37:14 -0300

    Criando processo de venda - P1

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/StatusRastreio.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/StatusRastreio.java
@@ -42,12 +42,11 @@ public class StatusRastreio implements Serializable {
 	
 	
 	
-	
-	@ManyToOne(targetEntity = Pessoa.class)
+	@ManyToOne(targetEntity = PessoaJuridica.class)
 	@JoinColumn(name = "empresa_id", nullable = false,
 	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, 
 	name = "empresa_id_fk"))
-	private Pessoa empresa;
+	private PessoaJuridica empresa;
 
 	
 	
@@ -57,10 +56,10 @@ public class StatusRastreio implements Serializable {
 	public void setVendaCompraLojaVirtual(VendaCompraLojaVirtual vendaCompraLojaVirtual) {
 		this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
 	}
-	public Pessoa getEmpresa() {
+	public PessoaJuridica getEmpresa() {
 		return empresa;
 	}
-	public void setEmpresa(Pessoa empresa) {
+	public void setEmpresa(PessoaJuridica empresa) {
 		this.empresa = empresa;
 	}
 	public Long getId() {
