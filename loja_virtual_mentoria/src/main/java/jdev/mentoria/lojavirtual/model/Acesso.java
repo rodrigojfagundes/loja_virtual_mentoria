ca9e42a140e41514ea2b84ecf359b6a920d52b2f
Rodrigo <Rodrigo@DESKTOP-NG2MGDC>
2025-05-03 22:17:45 -0300

    Criando Banco e Classes com Spring Data - P2

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/Acesso.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/Acesso.java
@@ -1,8 +1,5 @@
 package jdev.mentoria.lojavirtual.model;
 
-import java.io.Serializable;
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,61 +8,50 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-
+import org.springframework.lang.Nullable;
+import org.springframework.security.core.GrantedAuthority;
 
 
 @Entity
-@Table(name="marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", 
-sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
 
 	private static final long serialVersionUID = 1L;
-	
-
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,
-	generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
 	private Long id;
-	
-	@Column(nullable = false)
-	private String nomeDesc;
-	
-	
-	public MarcaProduto(Long id, String nomeDesc) {
-		this.id = id;
-		this.nomeDesc = nomeDesc;
-	}
 
 	
-	public MarcaProduto() {
-		this.id = id;
-		this.nomeDesc = nomeDesc;
-	}	
+	@Column(nullable = false)
+	private String descricacao;
+
 	
 	
+	
+	@Override
+	public String getAuthority() {
+		
+		return this.descricacao;
+	}
+
 	public Long getId() {
 		return id;
 	}
-	
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	
-	public String getNomeDesc() {
-		return nomeDesc;
-	}
-	
-	
-	public void setNomeDesc(String nomeDesc) {
-		this.nomeDesc = nomeDesc;
+
+	public String getDescricacao() {
+		return descricacao;
 	}
 
+	public void setDescricacao(String descricacao) {
+		this.descricacao = descricacao;
+	}
 
-	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -73,7 +59,6 @@ public class MarcaProduto implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -83,7 +68,7 @@ public class MarcaProduto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		Acesso other = (Acesso) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -92,7 +77,4 @@ public class MarcaProduto implements Serializable {
 		return true;
 	}
 
-
-	
-	
 }
