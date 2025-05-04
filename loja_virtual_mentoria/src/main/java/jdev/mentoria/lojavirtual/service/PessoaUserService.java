6af53380090434af274b034d1e2bcef1ffbfc2f2
Rodrigo <Rodrigo@DESKTOP-NG2MGDC>
2025-05-04 15:41:54 -0300

    Gravando produtos

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/service/PessoaUserService.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/service/PessoaUserService.java
@@ -192,23 +192,25 @@ public class PessoaUserService {
 		
 	}
 	
-	public CepDTO consultaCep(String cep) {
-		
-		
-		
-		return new RestTemplate().getForEntity(
-				"https://viacep.com.br/ws/" + cep +"/json/", CepDTO.class).getBody();
-	}
 	
-	public ConsultaCnpjDto consultaCnpjReceitaWS(String cnpj) {
 		
 		
-		
+		public CepDTO consultaCep(String cep) {
+			
+			
+			
 			return new RestTemplate().getForEntity(
-					"https://receitaws.com.br/v1/cnpj/" + cnpj, ConsultaCnpjDto.class).getBody();
-	
-	}
-
+					"https://viacep.com.br/ws/" + cep +"/json/", CepDTO.class).getBody();
+		}
+		
+		public ConsultaCnpjDto consultaCnpjReceitaWS(String cnpj) {
+			
+			
+			
+				return new RestTemplate().getForEntity(
+						"https://receitaws.com.br/v1/cnpj/" + cnpj, ConsultaCnpjDto.class).getBody();
+		
+		}
 	
 	
 }
